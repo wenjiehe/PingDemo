bd7c1fbaf26f24b4f36984f11321502de793319f
heweiqi123 <123610105@qq.com>
2020-01-14 01:44:15 +0800

    ..

--- PingDemo/ViewController.m
+++ PingDemo/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 #import "SimplePing.h"
+#include <netdb.h>
 
 @interface ViewController ()<SimplePingDelegate>
 
 @property(nonatomic,strong)SimplePing *ping;
+@property(nonatomic)dispatch_source_t timer;
 
 @end
 
@@ -20,6 +22,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self start:NO forceIPv6:NO];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self stop];
+}
+
+- (void)gcdTimer
+{
+    _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(_timer, dispatch_time(DISPATCH_TIME_NOW, 0), 1 * NSEC_PER_SEC, 5 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(_timer, ^{
+        //发送数据进行监测
+        [self.ping sendPingWithData:nil];
+    });
+    //启动定时器
+    dispatch_resume(_timer);
 }
 
 - (void)start:(BOOL)forceIPv4 forceIPv6:(BOOL)forceIPv6
@@ -30,6 +50,8 @@
         self.ping.addressStyle = SimplePingAddressStyleICMPv4;
     }else if (forceIPv6 && !forceIPv4){
         self.ping.addressStyle = SimplePingAddressStyleICMPv6;
+    }else{
+        self.ping.addressStyle = SimplePingAddressStyleAny;
     }
     
     self.ping.delegate = self;
@@ -40,6 +62,33 @@
 {
     [self.ping stop];
     self.ping = nil;
+    //暂停定时器
+    dispatch_suspend(_timer);
+    //取消定时器
+    dispatch_source_cancel(_timer);
+}
+
+- (void)displayAddress:(NSData *)address
+{
+    char hostStr[NI_MAXHOST];
+    
+    NSAssert(address != nil, @"address is nil");
+    int i = getnameinfo([address bytes], (socklen_t)[address length], hostStr, sizeof(hostStr), NULL, 0, NI_NUMERICHOST);
+    if (i == 0) {
+        NSLog(@"result = %@", [NSString stringWithCString:hostStr encoding:NSUTF8StringEncoding]);
+    }
+}
+
+- (void)showError:(NSError *)error
+{
+    NSAssert(error != nil, @"error is nil");
+    if ([error.domain isEqual:(__bridge NSString *)kCFErrorDomainCFNetwork] && error.code == kCFHostErrorUnknown) {
+        NSLog(@"error.userInfo = %@", error.userInfo);
+        NSNumber *addr = [[error userInfo] objectForKey:(id)kCFGetAddrInfoFailureKey];
+    }
+    if (error.localizedFailureReason) {
+        NSLog(@"error.localizedFailureReason = %@", error.localizedFailureReason);
+    }
 }
 
 #pragma mark -- SimplePingDelegate
@@ -48,7 +97,8 @@
 /// @param address IP地址或者域名
 - (void)simplePing:(SimplePing *)pinger didStartWithAddress:(NSData *)address
 {
-    
+    [self displayAddress:address];
+    [self gcdTimer];
 }
     
 /// 当发送失败时调用
@@ -56,16 +106,17 @@
 /// @param error 错误信息
 - (void)simplePing:(SimplePing *)pinger didFailWithError:(NSError *)error
 {
-    
+    [self showError:error];
+    [self.ping stop];
 }
 
-/// 发送ping数据包时连续调用
+/// 发送ping数据包成功时调用
 /// @param pinger 发送的实例对象
 /// @param packet 包含ICMP
 /// @param sequenceNumber ICMP序列号
 - (void)simplePing:(SimplePing *)pinger didSendPacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber
 {
-    
+    NSLog(@"#%u sent", sequenceNumber);
 }
 
 /// 发送数据包失败时调用
@@ -75,17 +126,23 @@
 /// @param error 错误信息
 - (void)simplePing:(SimplePing *)pinger didFailToSendPacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber error:(NSError *)error
 {
-    
+    [self showError:error];
+    NSLog(@"#%u send failed %@", sequenceNumber, error);
 }
 
+
+/// 发送数据包之后收到响应
+/// @param pinger 发送的实例对象
+/// @param packet 发送的数据包
+/// @param sequenceNumber 数据包中的ICMP序列号
 - (void)simplePing:(SimplePing *)pinger didReceivePingResponsePacket:(NSData *)packet sequenceNumber:(uint16_t)sequenceNumber
 {
-    
+    NSLog(@"%s #%u", __FUNCTION__, sequenceNumber);
 }
 
 - (void)simplePing:(SimplePing *)pinger didReceiveUnexpectedPacket:(NSData *)packet
 {
-    
+    NSLog(@"%s", __FUNCTION__);
 }
 
 
